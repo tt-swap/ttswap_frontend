35c4695193ffdc3d352e5eca9d1cd6d9af607932
DaJYu <1906681540@qq.com>
2025-07-17 22:13:27 +0800

    update contract address

--- data/contractConfig.ts
+++ data/contractConfig.ts
@@ -181,7 +181,7 @@ export const contractCon: {
   },
   560048: {
     chainId: "560048",
-    contractAddress: "0xd5CFC0b1e7651c7dCdA4d18801b6FECd95131979",
+    contractAddress: "0x85873c8F7f7dcA3628DC8D13A28c42C615f6Ec6f",
     theGraphApi: "https://api.studio.thegraph.com/query/57827/ttswap/version/latest",
     permit2Address:"0x9588F74Df5BbC1CD3a45720Cb944A4b1048A4450",
     weth9:"0x2387fD72C1DA19f6486B843F5da562679FbB4057"
