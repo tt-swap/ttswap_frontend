b6bc3cce870b0da2393e9b9210945159518d7f78
DaJYu <1906681540@qq.com>
2025-11-23 23:00:52 +0800

    update mainnet contract

--- data/contractConfig.ts
+++ data/contractConfig.ts
@@ -35,7 +35,7 @@ export const contractCon: {
 } = {
   1: {
     chainId: "1",
-    contractAddress: "0x5f06f5eCd628dbF79683e1e6fa8acdde8FA8C238",
+    contractAddress: "0x3f3bD120E6e214056b4f43d6682d850E2c889822",
     theGraphApi: "https://api.studio.thegraph.com/query/1685388/ttswap-ethereum/version/latest",
     permit2Address:"0x000000000022D473030F116dDEE9F6B43aC78BA3",
     weth9:"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
